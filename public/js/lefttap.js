105523fcef5e98c11bf8b6fff9f06ed14ef671ad
hskim <rhgustmfrh@naver.com>
2022-12-01 15:58:28 +0900

    "12-01 upt"

--- public/js/lefttap.js
+++ public/js/lefttap.js
@@ -7,6 +7,30 @@ const hbgMenu = document.querySelector(".hbgMenu");
 const downarrow = document.querySelectorAll(".downarrow");
 const rightarrow = document.querySelectorAll(".rightarrow");
 
+//resize 이벤트
+window.addEventListener("resize",function(){
+    hbgresize();
+});
+
+window.addEventListener("load",function(){
+    hbgresize();
+});
+
+function hbgresize (){
+    if(window.innerWidth <= 1000 && window.innerWidth > 0){
+        hbgMenu.classList.add("on");
+        hbgBtn.classList.add("on");
+        container.classList.add("on");
+        footer.classList.add("on");
+    }
+    else{
+        hbgMenu.classList.remove("on");
+        hbgBtn.classList.remove("on");
+        container.classList.remove("on");
+        footer.classList.remove("on");
+    }
+}
+
 //hbg드롭 다운 메뉴 등장
 depthWrap2.forEach(function(item,index){
     item.addEventListener("click",function(){
@@ -35,10 +59,23 @@ depthWrap2.forEach(function(item,index){
 const hbgBtn = document.querySelector(".hbgBtn");
 const container = document.querySelector("#container");
 const footer = document.querySelector("#footer");
+const bg = document.querySelector(".hbgMenu .bg");
 
 hbgBtn.addEventListener("click",function(){
-    hbgMenu.classList.toggle("on");
-    hbgBtn.classList.toggle("on");
-    container.classList.toggle("on");
-    footer.classList.toggle("on");
+    if(window.innerWidth <= 1000 && window.innerWidth > 0){
+        hbgMenu.classList.remove("on");
+        hbgMenu.style.backgroundColor = "#fff";
+        hbgMenu.style.width = "290px";
+        bg.style.display = "block";
+        bg.addEventListener("click",function(){
+            hbgMenu.classList.add("on");
+            bg.style.display = "none";
+        });
+    }
+    else{
+        hbgMenu.classList.toggle("on");
+        hbgBtn.classList.toggle("on");
+        container.classList.toggle("on");
+        footer.classList.toggle("on");  
+    }
 });
